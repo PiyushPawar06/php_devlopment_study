66a1fe447027e4804141b829ffa08d99313a8e6a
PiyushPawar06 <piyushpawar274@gmail.com>
2025-08-11 16:56:07 +0530

    PHP Day 1 - session 2

---
+++ basic_3.php
@@ -0,0 +1,16 @@
+<?php
+// php math funtions
+echo(pi()); # gives the values of pi
+
+echo (min(1,224,65477,5688,34,2,-12131314)); # tells the min of the arguments in this list
+echo (max(1,224,65477,5688,34,2,-12131314)); # tells the max of the arguments in this list
+echo (abs(-12131314)); # gives the absoulte positive number as a output
+
+echo (sqrt(16)); # gives the square root of the number as a output
+
+echo (round(16.646)); # gives the the closest round figure number as a output
+
+echo (rand(1,9)); # selects random number from the given range 
+
+
+?>
